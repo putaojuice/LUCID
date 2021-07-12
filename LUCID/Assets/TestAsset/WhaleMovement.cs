430ddf56d8fed420ae556951498465e1c09d0dfc
putaojuice <33406416+putaojuice@users.noreply.github.com>
2021-07-12 17:35:05 +0800

    Asset update - whale
    Added whale in the game scene for aesthetic. Unity's standard particle system assets were imported in order for ripple particle (self created) to work.

---
+++ LUCID/Assets/TestAsset/WhaleMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoint;
+    private int size;
+    private int pointer = 0;
+    private Transform currTarget;
+
+    public ParticleSystem ripple;
+
+    private float timer = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = waypoint.Length;
+        Next();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation
+            (Vector3.RotateTowards(transform.forward, transform.position - currTarget.position, Time.deltaTime * 30f, 0.0f));
+        transform.position = Vector3.MoveTowards(transform.position, currTarget.position, Time.deltaTime * 1f);
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            ParticleSystem particle = Instantiate(ripple, transform.position, Quaternion.LookRotation(new Vector3(0,90,0)));
+            timer = 1f;
+        }
+    }
+
+    private void Next()
+    {
+        currTarget = waypoint[pointer];
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (pointer >= size - 1)
+        {
+            pointer = 0;
+            Next();
+        }
+        else
+        {
+            pointer++;
+            Next();
+        }
+    }
+}
