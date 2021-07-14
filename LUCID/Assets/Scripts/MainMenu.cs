a79708046403123e394e7854261d15fd56f24af9
putaojuice <33406416+putaojuice@users.noreply.github.com>
2021-07-14 18:20:02 +0800

    Main menu update
    Added start button
    Added options menu
    Added audio listener in main menu
    Added slider to adjust master volume

--- LUCID/Assets/Scripts/MainMenu.cs
+++ LUCID/Assets/Scripts/MainMenu.cs
@@ -2,13 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
-	public void PlayGame ()
+	[SerializeField] private AudioMixer audioMixer;
+	public GameObject optionsMenu;
+
+	private void Start() 
+	{
+		optionsMenu.SetActive(false);	
+	}
+
+	public void PlayGame()
 	{
 		// SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		// GameLevel scene code from build setting
 		SceneManager.LoadScene(1);
+	}
+
+	public void CloseMenu()
+	{
+		optionsMenu.SetActive(false);
+	}
+
+	public void OnMenu()
+	{
+		optionsMenu.SetActive(true);
+	}
+
+	public void SetVolume(float vol)
+	{
+		audioMixer.SetFloat("Volume", vol);
 	}
 }
