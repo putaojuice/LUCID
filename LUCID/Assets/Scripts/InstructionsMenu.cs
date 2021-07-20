e2729ec3c6c903d84574717c5404baa57247ab01
putaojuice <33406416+putaojuice@users.noreply.github.com>
2021-07-20 17:09:10 +0800

    Update tutorial logic

--- LUCID/Assets/Scripts/InstructionsMenu.cs
+++ LUCID/Assets/Scripts/InstructionsMenu.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 public class InstructionsMenu : MonoBehaviour
 {
     [SerializeField] private GameObject[] images;
+    [SerializeField] private GameObject[] circles;
+    [SerializeField] private Color selectedColor;
+    [SerializeField] private Color notSelectedColor;
     private int pointer;
 
     // Start is called before the first frame update
     void Start()
     {
         pointer = 0;
-        gameObject.SetActive(false);
     }
 
     public void NextPage()
@@ -19,8 +21,10 @@ public class InstructionsMenu : MonoBehaviour
         if (pointer < images.Length-1)
         {
             images[pointer].SetActive(false);
+            circles[pointer].GetComponent<Image>().color = notSelectedColor;
             pointer++;
             images[pointer].SetActive(true);
+            circles[pointer].GetComponent<Image>().color = selectedColor;
         }
     }
 
@@ -29,14 +33,22 @@ public class InstructionsMenu : MonoBehaviour
         if (pointer > 0)
         {
             images[pointer].SetActive(false);
+            circles[pointer].GetComponent<Image>().color = notSelectedColor;
             pointer--;
             images[pointer].SetActive(true);
+            circles[pointer].GetComponent<Image>().color = selectedColor;
         }
     }
 
     public void OpenInstructions()
     {
         gameObject.SetActive(true);
+        images[pointer].SetActive(false);
+        circles[pointer].GetComponent<Image>().color = notSelectedColor;
+        pointer = 0;
+        images[pointer].SetActive(true);
+        circles[pointer].GetComponent<Image>().color = selectedColor;
+
     }
 
     public void CloseInstructions()
